339a40f5777e213ccce32918a538252fdbe4dda3
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-09-07 23:38:39 +0200

    Add socket.io-client dependency and implement real-time messaging functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,54 @@
 import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect(process.env.REACT_APP_SERVER_URL);
 
 function App() {
-  return <div className="App"></div>;
+  //Room State
+  const [room, setRoom] = useState("");
+
+  // Messages States
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const joinRoom = () => {
+    if (room) {
+      socket.emit("join_room", room);
+    }
+  };
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+    setMessage("");
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+  return (
+    <div className="App">
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <input
+        placeholder="Message..."
+        value={message}
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}> Send Message</button>
+      <h1> Message:</h1>
+      {messageReceived}
+    </div>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -15,8 +15,24 @@ const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
   },
+});
+
+// Catch each user trying to connect to the server separately.
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  // socket.on("send_message", (data) => {
+  //   socket.broadcast.emit("receive_message", data);
+  // });
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 });
 
 server.listen(PORT, () => {
