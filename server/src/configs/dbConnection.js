f948083b2cca3c73e6c9650913192c8d90f15e37
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-10-07 15:54:36 +0200

    Refactor dbConnection to simplify error handling and termination

--- server/src/configs/dbConnection.js
+++ server/src/configs/dbConnection.js
@@ -3,14 +3,11 @@ const mongoose = require("mongoose");
 const dbConnection = function () {
   // Connect:
   mongoose
-    .connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("* DB Connected * "))
     .catch((err) => {
       console.error("* DB Not Connected * ", err);
-      process.exit(1); // Uygulamayı durdurun
+      process.exit(1);
     });
 };
 
